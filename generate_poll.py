92f0e42afe727fdf11cfdcba1b213480ec8d56d8
walkersutton <walker.c.sutton@gmail.com>
2022-01-30 15:04:47 -0500

    Tweepy API -> Twitter API v2 - requerying a poll suggestion

--- generate_poll.py
+++ generate_poll.py
@@ -1,103 +1,78 @@
+from collections import defaultdict
 from heapq import heapify, heappush, nlargest
 from os import environ
 from time import sleep
+import requests
 import sys
-from tweepy import API, Cursor, OAuthHandler, RateLimitError, TweepError
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 
-CONSUMER_KEY = environ.get('CONSUMER_KEY')
-CONSUMER_SECRET = environ.get('CONSUMER_SECRET')
-ACCESS_KEY = environ.get('ACCESS_KEY')
-ACCESS_SECRET = environ.get('ACCESS_SECRET')
+BEARER_TOKEN = environ.get('BEARER_TOKEN') # need to add to github
 WEPOLLUS_USERNAME = 'wepollus'
 WEPOLLUS_PASSWORD = environ.get('WEPOLLUS_PASSWORD')
 WEPOLLUS_TWITTER_ID = '1248443462883704832'
 REPLY_PREFIX_LEN = len("@wepollus ")
 
-auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = API(auth)
+def bearer_oauth(r):
+    r.headers['Authorization'] = f'Bearer {BEARER_TOKEN}'
+    return r
 
-def get_poll_question():
-    """ returns a Twitter API status object (?) """
-    # finding the most recent poll setup status id
-    setup_id_str = api.user_timeline(id = WEPOLLUS_TWITTER_ID, count = 1, page = 1)[0].id_str
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
 
-    # finding the best submitted poll question
-    question = None
-    questions = Cursor(api.search, q=f'to:{WEPOLLUS_USERNAME}', since_id=setup_id_str, tweet_mode='extended').items()
-    while True:
-        try:
-            question = questions.next()
-            if hasattr(question, 'in_reply_to_status_id_str') and question.in_reply_to_status_id == int(setup_id_str):
-                if not question or (question.favorite_count > question.favorite_count):
-                    reply_count = 0
-                    replies = Cursor(api.search, q=f'to:{question.user.screen_name}' , since_id=int(question.id_str), tweet_mode='extended').items()
-                    while True:
-                        try:
-                            # verify that there are at least 2 submitted question choices
-                            if (reply_count >= 2):
-                                break
-                            else:
-                                reply = replies.next()
-                                if hasattr(reply, 'in_reply_to_status_id_str') and reply.in_reply_to_status_id == int(question.id_str):
-                                    reply_count += 1
-                        except RateLimitError as e:
-                            exit("Twitter api rate limit reached")
-                        except TweepError as e:
-                            exit("Tweepy error occured")
-                        except StopIteration:
-                            break
-                        except Exception as e:
-                            exit("Failed while verifying question reply count")
-                    if reply_count >= 2:
-                        question.full_text = question.full_text[REPLY_PREFIX_LEN:]
-                        question = question
-        except RateLimitError as e:
-            exit("Twitter api rate limit reached")
-        except TweepError as e:
-            exit("Tweepy error occured")
-        except StopIteration:
-            print("stopped iteration")
+def last_query_tweet_id():
+    url = f'https://api.twitter.com/2/users/{WEPOLLUS_TWITTER_ID}/tweets'
+    params = {'max_results': 15}
+    resp = connect_to_endpoint(url, params)
+    query_tweet_id = None
+    for tweet in resp['data']:
+        if 'New Daily Poll' in tweet['text']:
+            query_tweet_id = tweet['id']
             break
-        except Exception as e:
-            exit("Failed while fetching replies")
+    return query_tweet_id
 
-    # remove the polling tweet if there wasn't enough engagement - eventually will remove for all polling tweets
-    if not question:
-        api.destroy_status(api.user_timeline(count=1)[0].id)
-    return question
-    
+def valid_suggestions():
+    """ Returns a list of valid poll suggestions in descending order of popularity:
+    [
+        {
+            'question': str,
+            'choices: [
+                {
+                    'chioce': str,
+                }, ...
+            ]
+        }, ...
+    ]
+    """
+    conversation_id = last_query_tweet_id()
+    url = 'https://api.twitter.com/2/tweets/search/recent'
+    params = {
+        'query': f'conversation_id:{conversation_id}',
+        'tweet.fields': 'conversation_id,public_metrics,referenced_tweets'
+    }
+    resp = connect_to_endpoint(url, params)
+    conversation_tweets = defaultdict(list)
+    for tweet in resp['data']:
+        conversation_tweets[tweet['referenced_tweets'][0]['id']].append(tweet)
 
-def get_poll_choices(question):
-    """ returns the 4 best poll choices as a list[str] """
-    # finding the best choices for the poll
-    choices = []
-    heapify(choices)
-    suggestions = Cursor(api.search, q=f'to:{question.user.screen_name}' , since_id=int(question.id_str), tweet_mode='extended').items()
-    while True:
-        try:
-            choice = suggestions.next()
-            if hasattr(choice, 'in_reply_to_status_id_str') and choice.in_reply_to_status_id == int(question.id_str):
-                reply_offset = 0
-                for user in choice.entities['user_mentions']:
-                    reply_offset += (2 + len(user['screen_name']))
-                choice_text = choice.full_text[reply_offset:]
-                if (len(choice_text) <= 25):
-                    heappush(choices, (int(choice.favorite_count), choice_text))
-        except RateLimitError as e:
-            exit("Twitter api rate limit reached")
-        except TweepError as e:
-            exit("Tweepy error occured")
-        except StopIteration:
-            break
-        except Exception as e:
-            exit("Failed while fetching choices")
+    suggestions = []
+    heapify(suggestions)
+    for question_tweet in conversation_tweets[conversation_id]:
+        question_text, question_likes = question_tweet['text'], question_tweet['public_metrics']['like_count']
+        choices = []
+        heapify(choices)
+        for choice_tweet in conversation_tweets[question_tweet['id']]:
+            choice_text, choice_likes = choice_tweet['text'], choice_tweet['public_metrics']['like_count']
+            heappush(choices, (choice_likes, choice_text))
+        if len(choices) >= 2:
+            best_choices = [choice[1] for choice in nlargest(4, choices)]
+            heappush(suggestions, (-1 * question_likes, question_text, best_choices))
 
-    choices = [choice[1] for choice in nlargest(4, choices)]
-    return choices
+    return [{'question': suggestion[1], 'choices': suggestion[2]} for suggestion in suggestions]
 
 def create_poll(question, choices):
     """ crates a Twitter poll using the given quesiton and choices """
@@ -138,9 +113,9 @@ def create_poll(question, choices):
             driver.find_element_by_xpath(f"//*[@name='Choice{ii + 1}']").send_keys(choices[ii])
 
             # tweet!
-            polling_tweet_id = api.user_timeline(count=1)[0].id
+            # polling_tweet_id = api.user_timeline(count=1)[0].id
             driver.find_element_by_xpath("//*[@name='Tweet']").click()
-            api.destroy_status(polling_tweet_id)
+            # api.destroy_status(polling_tweet_id)
 
     except Exception as e:
         driver.quit()
@@ -151,12 +126,11 @@ def create_poll(question, choices):
     sys.exit()
 
 
-
 def run_poll():
-    poll_question = get_poll_question()
+    suggestions = valid_suggestions()
 
-    if poll_question:
-        poll_choices = get_poll_choices(poll_question)
-        create_poll(poll_question, poll_choices)
-    else:
-        print('lack of engagement means no poll for today :(')
+    # if poll_question:
+    #     poll_choices = get_poll_choices(poll_question)
+    #     create_poll(poll_question, poll_choices)
+    # else:
+    #     print('lack of engagement means no poll for today :(')
