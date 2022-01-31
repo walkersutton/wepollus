25424b463cd0a1c5cd146c7f2f0b184649fa45c5
walkersutton <walker.c.sutton@gmail.com>
2022-01-30 23:53:40 -0500

    implement tweeting with Twitter API v2

--- poll_a_poll.py
+++ poll_a_poll.py
@@ -1,16 +1,24 @@
+from http.client import REQUESTED_RANGE_NOT_SATISFIABLE
 from os import environ
-import sys
-from tweepy import API, OAuthHandler
+import requests
+from requests_oauthlib import OAuth1
 
 CONSUMER_KEY = environ.get('CONSUMER_KEY')
 CONSUMER_SECRET = environ.get('CONSUMER_SECRET')
 ACCESS_KEY = environ.get('ACCESS_KEY')
 ACCESS_SECRET = environ.get('ACCESS_SECRET')
 
-auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = API(auth)
+def request_poll_suggestions():
+    url = 'https://api.twitter.com/2/tweets'
+    data = {
+        'text': "New Daily Poll\n - favorite replies to select tomorrow's poll question\n - top resps on the most favorited reply will be the poll's choices"
+    }
+    resp = requests.post(url, json=data, auth=OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET))
 
-api.update_status("New Daily Poll\n - favorite replies to select tomorrow's poll question\n - top responses on the most favorited reply will be the poll's choices")
+    if not resp.status_code in (200, 201):
+        raise Exception(resp.status_code, resp.text)
 
-sys.exit()
+    return resp.json()
+
+if __name__ == '__main__':
+    request_poll_suggestions()
